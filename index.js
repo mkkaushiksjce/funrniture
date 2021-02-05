aa9afd47197457f093410ae340e96bae003e866d
mkkaushiksjce <mkkaushiksjce@gmail.com>
2021-02-05 19:47:13 +0530

    5 Types of carousel completed

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ class App extends Component {
   };
 
   render() {
-    const normal = [
+    const normal_sofas = [
       <img
         src="https://raw.githubusercontent.com/mkkaushiksjce/furniture/master/1.jpg"
         onDragStart={this.handleDragStart}
@@ -42,9 +42,97 @@ class App extends Component {
       />
     ];
 
+    const double_sofas = [
+      <img
+        src="https://raw.githubusercontent.com/mkkaushiksjce/furniture/master/1_double.jpg"
+        onDragStart={this.handleDragStart}
+      />,
+      <img
+        src="https://raw.githubusercontent.com/mkkaushiksjce/furniture/master/2_double.jpg"
+        onDragStart={this.handleDragStart}
+      />,
+      <img
+        src="https://raw.githubusercontent.com/mkkaushiksjce/furniture/master/3_double.jpg"
+        onDragStart={this.handleDragStart}
+      />,
+      <img
+        src="https://raw.githubusercontent.com/mkkaushiksjce/furniture/master/4_double.jpg"
+        onDragStart={this.handleDragStart}
+      />,
+      <img
+        src="https://raw.githubusercontent.com/mkkaushiksjce/furniture/master/5_double.jpg"
+        onDragStart={this.handleDragStart}
+      />
+    ];
+
+    const triple_sofas = [
+      <img
+        src="https://raw.githubusercontent.com/mkkaushiksjce/furniture/master/1_three.jpg"
+        onDragStart={this.handleDragStart}
+      />,
+      <img
+        src="https://raw.githubusercontent.com/mkkaushiksjce/furniture/master/2_three.jpg"
+        onDragStart={this.handleDragStart}
+      />,
+      <img
+        src="https://raw.githubusercontent.com/mkkaushiksjce/furniture/master/3_three.jpg"
+        onDragStart={this.handleDragStart}
+      />,
+      <img
+        src="https://raw.githubusercontent.com/mkkaushiksjce/furniture/master/4_three.jpg"
+        onDragStart={this.handleDragStart}
+      />,
+      <img
+        src="https://raw.githubusercontent.com/mkkaushiksjce/furniture/master/5_three.jpg"
+        onDragStart={this.handleDragStart}
+      />
+    ];
+
+    const sectional_sofas = [
+      <img
+        src="https://raw.githubusercontent.com/mkkaushiksjce/furniture/master/1_sectional.jpg"
+        onDragStart={this.handleDragStart}
+      />,
+      <img
+        src="https://raw.githubusercontent.com/mkkaushiksjce/furniture/master/2_sectional.jpg"
+        onDragStart={this.handleDragStart}
+      />,
+      <img
+        src="https://raw.githubusercontent.com/mkkaushiksjce/furniture/master/3_sectional.jpg"
+        onDragStart={this.handleDragStart}
+      />
+    ];
+
+    const pouf_sofas = [
+      <img
+        src="https://raw.githubusercontent.com/mkkaushiksjce/furniture/master/1_pouf.jpg"
+        onDragStart={this.handleDragStart}
+      />,
+      <img
+        src="https://raw.githubusercontent.com/mkkaushiksjce/furniture/master/2_pouf.jpg"
+        onDragStart={this.handleDragStart}
+      />,
+      <img
+        src="https://raw.githubusercontent.com/mkkaushiksjce/furniture/master/3_pouf.jpg"
+        onDragStart={this.handleDragStart}
+      />,
+      <img
+        src="https://raw.githubusercontent.com/mkkaushiksjce/furniture/master/4_pouf.jpg"
+        onDragStart={this.handleDragStart}
+      />,
+      <img
+        src="https://raw.githubusercontent.com/mkkaushiksjce/furniture/master/5_pouf.jpg"
+        onDragStart={this.handleDragStart}
+      />
+    ];
+
     return (
       <div>
-        <AliceCarousel mouseTracking items={normal} />
+        <AliceCarousel mouseTracking items={normal_sofas} />
+        <AliceCarousel mouseTracking items={double_sofas} />
+        <AliceCarousel mouseTracking items={triple_sofas} />
+        <AliceCarousel mouseTracking items={sectional_sofas} />
+        <AliceCarousel mouseTracking items={pouf_sofas} />
       </div>
     );
   }
